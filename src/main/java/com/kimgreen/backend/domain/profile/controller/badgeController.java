707142b50b2c822ab7f88fc3f19e6143dd5c8b8e
gol2580 <gol11971197@gmail.com>
2024-01-11 20:37:58 +0900

    [feat] changeRepresentativeBadge

--- src/main/java/com/kimgreen/backend/domain/profile/controller/badgeController.java
+++ src/main/java/com/kimgreen/backend/domain/profile/controller/badgeController.java
@@ -1,12 +1,34 @@
 package com.kimgreen.backend.domain.profile.controller;
 
+import com.kimgreen.backend.domain.member.dto.Auth.SignUpRequestDto;
+import com.kimgreen.backend.domain.member.repository.MemberRepository;
+import com.kimgreen.backend.domain.profile.dto.RepBadgeRequestDto;
+import com.kimgreen.backend.domain.profile.repository.BadgeRepository;
+import com.kimgreen.backend.domain.profile.service.BadgeService;
+import com.kimgreen.backend.response.Response;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static com.kimgreen.backend.response.Message.SIGN_UP_SUCCESS;
+import static com.kimgreen.backend.response.Response.success;
+import static org.springframework.http.HttpStatus.OK;
 
 @Tag(name = "badge")
 @RestController
+@AllArgsConstructor
 @RequestMapping(value="/badge")
 public class badgeController {
+
+    private final BadgeService badgeService;
+
+    @Operation(summary = "대표뱃지 설정하기")
+    @ResponseStatus(OK)
+    @PatchMapping()
+    public Response changeRepBadge(@RequestBody RepBadgeRequestDto repBadgeRequestDto) {
+        badgeService.changeRepBadge(repBadgeRequestDto);
+        return success(SIGN_UP_SUCCESS);
+    }
 }
---
+++ src/main/java/com/kimgreen/backend/domain/profile/dto/RepBadgeRequestDto.java
@@ -0,0 +1,13 @@
+package com.kimgreen.backend.domain.profile.dto;
+
+import com.kimgreen.backend.domain.BadgeList;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RepBadgeRequestDto {
+    private String badgeName;
+}
