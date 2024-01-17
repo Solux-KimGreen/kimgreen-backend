61b2b04a9692cfc54ca14e09f31a3f32682778bd
gol2580 <gol11971197@gmail.com>
2024-01-18 02:04:07 +0900

    [fix] docker-compose.yml

--- src/main/java/com/kimgreen/backend/domain/community/repository/PostRepository.java
+++ src/main/java/com/kimgreen/backend/domain/community/repository/PostRepository.java
@@ -1,11 +1,12 @@
 package com.kimgreen.backend.domain.community.repository;
 
-import com.kimgreen.backend.domain.community.entity.Category;
-import com.kimgreen.backend.domain.community.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+        import com.kimgreen.backend.domain.community.entity.Post;
+        import org.springframework.data.jpa.repository.JpaRepository;
+        import org.springframework.stereotype.Repository;
+
+        import java.time.LocalDateTime;
+        import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-    int countByCategory(Category category); //해당 카테고리에 속한 게시글 수 조회
 }
