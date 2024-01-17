7f09cf6835711f1dcb07ad034378dba649bfc130
gol2580 <gol11971197@gmail.com>
2024-01-18 02:04:07 +0900

    [fix] docker-compose.yml

--- src/main/java/com/kimgreen/backend/domain/community/repository/PostRepository.java
+++ src/main/java/com/kimgreen/backend/domain/community/repository/PostRepository.java
@@ -1,9 +1,11 @@
 package com.kimgreen.backend.domain.community.repository;
 
-import com.kimgreen.backend.domain.community.entity.Category;
-import com.kimgreen.backend.domain.community.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+        import com.kimgreen.backend.domain.community.entity.Post;
+        import org.springframework.data.jpa.repository.JpaRepository;
+        import org.springframework.stereotype.Repository;
+
+        import java.time.LocalDateTime;
+        import java.util.List;
 
 import java.time.LocalDateTime;
 import java.util.List;
