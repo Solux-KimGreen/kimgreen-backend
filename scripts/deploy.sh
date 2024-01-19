ae7d1b306619b3a6ce7d6e7d00c136884fd65ec8
gol2580 <gol11971197@gmail.com>
2024-01-20 03:02:03 +0900

    [CI] code-deploy.yml

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@ REPOSITORY=/home/ec2-user/app
 cd $REPOSITORY
 
 APP_NAME=kimgreen
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | head -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 CURRENT_PID=$(pgrep -f $APP_NAME)
